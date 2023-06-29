3c280958cddbee2895974e8facb9bb0e83c469fc
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-06-29 05:51:37 -0500

    fixing cart

--- frontend/src/components/checkout/Cart.js
+++ frontend/src/components/checkout/Cart.js
@@ -1,41 +1,27 @@
-import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { Typography, List, ListItem, ListItemText } from '@mui/material';
-
-const products = [
-  {
-    name: 'Product 1',
-    desc: 'A nice thing',
-    price: '$9.99',
-  },
-  {
-    name: 'Product 2',
-    desc: 'Another thing',
-    price: '$3.45',
-  },
-  {
-    name: 'Product 3',
-    desc: 'Something else',
-    price: '$6.51',
-  },
-  {
-    name: 'Product 4',
-    desc: 'Best thing of all',
-    price: '$14.11',
-  }
-];
 
 
 function Cart() {
+
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    fetch('/orders')
+    .then(r=>r.json())
+    .then(orderData => setOrders(orderData))
+  }, [])
+
   return (
     <>
       <Typography variant="h6" gutterBottom>
         Order summary
       </Typography>
       <List disablePadding>
-        {products.map((product) => (
-          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-            <ListItemText primary={product.name} secondary={product.desc} />
-            <Typography variant="body2">{product.price}</Typography>
+        {orders.map((order) => (
+          <ListItem key={order.name} sx={{ py: 1, px: 0 }}>
+            <ListItemText primary={order.name} secondary={order.desc} />
+            <Typography variant="body2">{order.price}</Typography>
           </ListItem>
         ))}
         <ListItem sx={{ py: 1, px: 0 }}>
